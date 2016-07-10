2e56819e070832a44ad904796178b49167e8fafd
Thangaprabhu Chandrasekhar <TChandrasekhar@walmartlabs.com>
2016-07-10 16:32:41 -0700

    Running Fine

--- WebContent/html/callUserService.js
+++ WebContent/html/callUserService.js
@@ -1,7 +1,7 @@
 var myApp = angular.module("myModule",[]);
 
 var controller = function($scope, $http) {
-    $http.get('http://localhost:8080/Spring4MVCCRUDRestService/user/1').
+    $http.get('http://localhost:8080/SpringRestWithAngular/user/1').
     success(function(data) {
         $scope.user = data;
     });
